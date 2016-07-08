5d085f7982ef7058347e1b049910531bdd3d9b5c
imunew <imunew@gmail.com>
2016-07-08 11:27:49 +0900

    Fixing namespace.

--- lib/fluent/plugin/parser_monolog.rb
+++ lib/fluent/plugin/parser_monolog.rb
@@ -1,46 +1,50 @@
 require 'json'
 
-class Fluent::MonologParser < Fluent::Parser
-  Fluent::Plugin.register_parser('monolog', self)
+module Fluent
+  class TextParser
+    class MonologParser < Parser
+      Fluent::Plugin.register_parser('monolog', self)
 
-  REGEXP = /^\[(?<time>[\d\-]+ [\d\:]+)\] (?<channel>.+)\.(?<level>(DEBUG|INFO|NOTICE|WARNING|ERROR|CRITICAL|ALERT|EMERGENCY))\: (?<message>.*) (?<context>\{.+\}) \[(?<extra>.*)\]$/
-  TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+      REGEXP = /^\[(?<time>[\d\-]+ [\d\:]+)\] (?<channel>.+)\.(?<level>(DEBUG|INFO|NOTICE|WARNING|ERROR|CRITICAL|ALERT|EMERGENCY))\: (?<message>.*) (?<context>\{.+\}) \[(?<extra>.*)\]$/
+      TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-  def initialize
-    super
-    @time_parser = TimeParser.new(TIME_FORMAT)
-    @mutex = Mutex.new
-  end
+      def initialize
+        super
+        @time_parser = TimeParser.new(TIME_FORMAT)
+        @mutex = Mutex.new
+      end
 
-  def patterns
-    {'format' => REGEXP, 'time_format' => TIME_FORMAT}
-  end
+      def patterns
+        {'format' => REGEXP, 'time_format' => TIME_FORMAT}
+      end
 
-  def parse(text)
-    m = REGEXP.match(text)
-    unless m
-      yield nil, nil
-      return
+      def parse(text)
+        m = REGEXP.match(text)
+        unless m
+          yield nil, nil
+          return
+        end
+
+        time = m['time']
+        time = @mutex.synchronize { @time_parser.parse(time) }
+
+        channel = m['channel']
+        level = m['level']
+        message = m['message']
+        context = JSON.parse(m['context'])
+        extra = m['extra']
+
+        record = {
+            "channel" => channel,
+            "level" => level,
+            "message" => message,
+            "context" => context,
+            "extra" => extra
+        }
+        record["time"] = m['time'] if @keep_time_key
+
+        yield time, record
+      end
     end
-
-    time = m['time']
-    time = @mutex.synchronize { @time_parser.parse(time) }
-
-    channel = m['channel']
-    level = m['level']
-    message = m['message']
-    context = JSON.parse(m['context'])
-    extra = m['extra']
-
-    record = {
-        "channel" => channel,
-        "level" => level,
-        "message" => message,
-        "context" => context,
-        "extra" => extra
-    }
-    record["time"] = m['time'] if @keep_time_key
-
-    yield time, record
   end
 end
